253b34eb85c58321f308def6474dcbbad9f2dfa3
Nandini <nandinig819@gmail.com>
2022-11-22 19:40:14 +0530

    implemented routes in nextjs

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/Head";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>Web dev newz</title>
+        <meta name='keywords' content='web development, programming'/>
+      </Head>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default about;
